75bbb2c62acf6b1942f2598f3d13c81664454dc5
cathyvu <catherine.vu.97@hotmail.com>
2017-06-13 15:15:52 +1000

    Edited docstrings
    Same changes

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -11,13 +11,13 @@ import string
 
 
 def getLetter(index):
-    """S."""
+    """Returning an alphabet."""
     alphabet = string.ascii_lowercase + " "
     return alphabet[index]
 
 
 def week2exersise2():
-    """S."""
+    """Returning a secret word."""
     indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
     wordArray = map(getLetter, indices)
     wordArray[0] = wordArray[0].upper()
